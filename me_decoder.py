d588b849a63b3e0a2514516395c25cb40ffe9bc1
Ricardo Gamboa <ricgamboa@yahoo.com>
2020-04-11 15:51:22 -0500

    First decoder version

--- me_decoder.py
+++ me_decoder.py
@@ -1,7 +1,7 @@
 # Main program used to request the decryption of the answer
 # This module must run in a secure environment since it uses private user information
 
-import def_classes, client_communicator, rw_database, translator
+import me_components, me_decoder_client_communicator, me_get_sender_info, me_translator
 
 
 def main():
@@ -10,18 +10,18 @@ def main():
     ALPHABET_FILE = "public_alphabet"
 
     # Wait for request to decrypt an answer
-    communicator_client = client_communicator.ClientCommunicator()
+    communicator_client = me_decoder_client_communicator.ClientCommunicator()
     communicator_client.wait_for_request()
 
     # Receive the original question, the user id, and the answer to decrypt
     communicator_client.receive_request_info()
 
     # Get the private user information
-    user = def_classes.Sender(0,'')
-    [user.id, user.name, user.icons, user.cell] = rw_database.get_sender_info(communicator_client.senderid)
+    user = me_components.Sender(0, '')
+    [user.id, user.name, user.icons, user.cell] = me_get_sender_info.get_sender_info(communicator_client.senderid)
 
     # Read the public alphabet
-    current_translator = translator.Translator(ALPHABET_SIZE)
+    current_translator = me_translator.Translator(ALPHABET_SIZE)
     current_translator.set_alphabet(ALPHABET_FILE)
 
     # Find the icon used for each letter
